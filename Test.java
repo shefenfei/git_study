29a3f529c024adbcd8247b3ff48e8095fd0a3c0b
shefenfei <shefenfei@wanda.cn>
2017-04-11 11:07:40 +0800

    [Update] 修改文件 [Desc] 修改文件

--- Test.java
+++ Test.java
@@ -13,4 +13,14 @@ public class Test {
     public void setName(String name) {
         this.name = name;
     }
+
+    private int age;
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+
+    public int getAge() {
+      return age;
+    }
 }
