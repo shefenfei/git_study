086c88721fb0490afe6173083be6610d73beeb6c
shefenfei <shefenfei@wanda.cn>
2017-04-11 11:05:15 +0800

    [Add] 添加新的java文件 [Desc] 添加新的java文件

---
+++ Test.java
@@ -0,0 +1,16 @@
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by shefenfei on 2017/2/17.
+ */
+public class Test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
